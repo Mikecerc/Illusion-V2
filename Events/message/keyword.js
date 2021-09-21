5c56819acb3cfad790f0357f3cdf6dafbf90a115
Mikecerc1 <mikecerc1@outlook.com>
2021-09-21 16:09:50 -0400

    trying to fix keyword

--- Events/message/keyword.js
+++ Events/message/keyword.js
@@ -1,12 +1,11 @@
 module.exports = {
     name: 'keyword',
-    /**
-     * @param {Client} client
-     * @param {Message} message
-     */
+
+
     async execute(message) {
+        console.log(message.content);
         if (message.content.includes('I want to join programming')) {
-            return message.reply(`You cant legally say that here. Shame on you <@${message.author.id}>`);
+            return message.channel.send(`You cant legally say that here. Shame on you <@${message.author.id}>`);
         }
     },
 };
