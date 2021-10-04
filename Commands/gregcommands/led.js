c578fdce5efe0f57775a5632fc604670c782fb87
Mikecerc1 <mikecerc1@outlook.com>
2021-10-03 21:29:39 -0400

    minor changes

--- Commands/gregcommands/led.js
+++ Commands/gregcommands/led.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "leeeed",
+    name: "led",
     description: "LIGHT EMITTING DIODE",
     async execute(interaction) {
         interaction.channel.send('https://media1.tenor.com/images/e782e9078c7f4762f3f5d238416c811b/tenor.gif?itemid=23096778');
