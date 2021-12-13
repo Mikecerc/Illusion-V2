75f839657d21a38ae222e0cf2a0dfc305a7a48fb
Mikecerc1 <mikecerc1@outlook.com>
2021-12-12 22:12:28 -0500

    sashvat

--- Events/ayush.js
+++ Events/ayush.js
@@ -1,13 +1,32 @@
 module.exports = {
-    name: 'guildMemberUpdate',
-    async execute(oldMember, newMember) {
-        console.log(`new member: ${newMember.nickname}`,`old member: ${oldMember.nickname}`);
-        if (newMember.user.id != '697198271281758379') return;
-        if (newMember.nickname == null) return newMember.setNickname('Traitor').catch(err => console.log(err));
-        if (newMember.nickname.toLowerCase() === 'traitor') {
-            return;
-        } else {
-            newMember.setNickname('Traitor').catch(err => console.log(err)); 
-        }
+  name: "guildMemberUpdate",
+  async execute(oldMember, newMember) {
+    console.log(
+      `new member: ${newMember.nickname}`,
+      `old member: ${oldMember.nickname}`
+    );
+    if (newMember.user.id == "697198271281758379") {
+      if (newMember.nickname == null)
+        return newMember
+          .setNickname("Traitor")
+          .catch((err) => console.log(err));
+      if (newMember.nickname.toLowerCase() === "traitor") {
+        return;
+      } else {
+        newMember.setNickname("Traitor").catch((err) => console.log(err));
+      }
+    } else if (newMember.user.id == "692559110675103784") {
+        if (newMember.nickname == null)
+        return newMember
+          .setNickname("Disgrace to humanity")
+          .catch((err) => console.log(err));
+      if (newMember.nickname.toLowerCase() === "disgrace to humanity") {
+        return;
+      } else {
+        newMember.setNickname("Disgrace to humanity").catch((err) => console.log(err));
+      }
+    } else {
+        return;
     }
-}
+  },
+};
