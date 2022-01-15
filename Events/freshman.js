c49bd057d1ecc24a1b436130d069cf89b3fb5866
Mikecerc1 <mikecerc1@outlook.com>
2022-01-15 18:28:11 -0500

    made Kyle's trigger words a json file

--- Events/freshman.js
+++ Events/freshman.js
@@ -1,14 +1,32 @@
-const { MessageEmbed } =require('discord.js');
+const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
 module.exports = {
-    name: 'messageCreate', 
-    execute (message) {
-        if (message.author.id != '406629388059410434') return;
-        let content = message.content.toLowerCase().replace(/\s/g, '')
-        
-            const Response = new MessageEmbed()
-                .setColor('RED')
-                .setDescription('<@406629388059410434> stop acting like a freshman');
-            message.reply({ embeds: [Response] });
-        
+  name: "messageCreate",
+  execute(message) {
+    if (message.author.id != "406629388059410434") return;
+    let content = message.content.toLowerCase().replace(/\s/g, "");
+    fs.readFile("../json/triggerwords.json", "utf-8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        if (testWords(JSON.parse(data), content) == true) {
+          const Response = new MessageEmbed()
+            .setColor("RED")
+            .setDescription(
+              "<@406629388059410434> stop acting like a freshman"
+            );
+          message.reply({ embeds: [Response] });
+        }
+      }
+    });
+    async function testWords(data, content) {
+        let iteration = 0;
+        for(let words in data) {
+            if (content.includes(data[iteration])) {
+                return true;
+            }
+            iteration++; 
+        }
     }
-}
+  },
+};
