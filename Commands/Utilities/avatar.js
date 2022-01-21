c517f5aaf200fb7b9caf75d1fb0eb7e7f0a98b18
Mikecerc1 <mikecerc1@outlook.com>
2022-01-20 23:50:48 -0500

    added avatar command

---
+++ Commands/Utilities/avatar.js
@@ -0,0 +1,25 @@
+const { MessageEmbed } = require('discord.js');
+module.exports = {
+    name: 'avatar',
+    description: 'gets a users avatar',
+    options:[
+        {
+            name: 'target',
+            description: 'Select a target',
+            type: 'USER',
+            required: true,
+        },
+    ],
+    execute(interaction) {
+        const target = interaction.options.getUser('target');
+
+        const response = new MessageEmbed()
+        .setAuthor(`${target.user.username}`, target.user.displayAvatarURL({ dynamic: true }))
+        .setTitle(`${target.user.username}'s avatar`)
+        .setImage(target.user.displayAvatarURL({ dynamic: true }))
+        .setFooter(`Requested By ${interaction.user.tag}`);
+
+        interaction.followUp('done');
+        return interaction.channel.send({ embeds: [response] });
+    }
+}
