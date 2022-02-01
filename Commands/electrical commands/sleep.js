f3307ff50eb9555607484b0de87f2ef7879398c4
Mikecerc1 <mikecerc1@outlook.com>
2022-01-31 23:19:47 -0500

    sleep

---
+++ Commands/electrical commands/sleep.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'sleep',
+    description: 'just play it',
+    execute(interaction) {
+        return interaction.reply('https://www.youtube.com/watch?v=teIbh8hFQos');
+    }
+}
