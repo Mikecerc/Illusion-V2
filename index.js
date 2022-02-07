b9db041fd7db49c332a27af732548048cccea5ba
Mikecerc1 <mikecerc1@outlook.com>
2022-02-06 21:35:26 -0500

    oops

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: 32767 });
-module.export = client;
+module.exports = client;
 const { token } = require('./config.json');
 
 client.commands = new Collection();
