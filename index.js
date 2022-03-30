f15a1d1c618312ded359ea007897d9bfc4ae9e91
Mikecerc1 <Mikecerc1@outlook.com>
2022-03-30 16:26:29 -0400

    major file system changes, .env, tba data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
+//mongoose.connect(process.env.db).then(console.log('connected to db')).catch((err) => console.log(err))
 const { Client, Collection } = require('discord.js');
 const client = new Client({ intents: 32767 });
 module.exports = client;
