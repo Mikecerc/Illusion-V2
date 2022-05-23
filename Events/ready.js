4872cb9c80143141411b4972ae35d4379aad5351
Mikecerc1 <Mikecerc1@outlook.com>
2022-05-23 13:12:28 -0400

    changed activity status

--- Events/ready.js
+++ Events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'ready',
     execute(client) {
         console.log('The client is ready :)');
-        client.user.setActivity('my spindexer breaking', { type: 'LISTENING' });
+        client.user.setActivity('Recycle Rush Videos', { type: 'WATCHING' });
     },
 };
