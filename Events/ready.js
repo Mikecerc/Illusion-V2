26a6ff0059f4a5f9cc4fda6241c89336b6ea7dab
Mikecerc1 <Mikecerc1@outlook.com>
2022-06-02 13:25:28 -0400

    finished events

--- Events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'ready',
-    execute(client) {
-        console.log('The client is ready :)');
-        client.user.setActivity('Recycle Rush Videos', { type: 'WATCHING' });
-    },
-};
