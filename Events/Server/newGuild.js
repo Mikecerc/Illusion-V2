e60ec301d68e73d7b31b32076620947f4725e858
Mikecerc1 <mikecerc1@outlook.com>
2022-06-10 23:07:29 -0400

    removed unnecessary file

--- Events/Server/newGuild.js
+++
@@ -1,9 +0,0 @@
-const client = require('../../index.js')
-module.exports = {
-    name: 'guildCreate',
-    async execute() {
-        setTimeout(() => require('../../Handlers/Commands.js')(client), 5000)
-        console.log('the bot has been added to a new guild')
-        console.log(client.guilds.cache.map(res => {return res }))
-    },
-};
