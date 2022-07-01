12a5f25208b03de3a9080dadc96e9f82d563a689
Mikecerc1 <mikecerc1@outlook.com>
2022-06-30 23:38:01 -0400

    fixed bug in user info

--- Commands/Utilities/userinfo.js
+++ Commands/Utilities/userinfo.js
@@ -1,31 +1,64 @@
-const { MessageEmbed } = require('discord.js');
-const moment = require('moment');
+const { MessageEmbed } = require("discord.js");
+const moment = require("moment");
 
 module.exports = {
-    name: 'dox',
-    description: 'Gets a users info and avatar',
-    options:[
+    name: "dox",
+    description: "Gets a users info and avatar",
+    options: [
         {
-            name: 'target',
-            description: 'Select a target',
-            type: 'USER',
+            name: "target",
+            description: "Select a target",
+            type: "USER",
             required: true,
         },
     ],
 
     async execute(interaction) {
         await interaction.deferReply({ ephemeral: false }).catch(() => {});
-        const Target = interaction.options.getMember('target');
-        console.log(Target.user.displayAvatarURL)
-        const Response = new MessageEmbed()
-        .setAuthor({name: `${Target.user.username}`, iconURL: Target.user.displayAvatarURL({ dynamic: true })})
-        .setThumbnail(Target.user.displayAvatarURL({ dynamic: true }))
-        .setColor('RANDOM')
-        .addField('UserID', `${Target.user.id}`, false)
-        .addField('Roles', `${Target.roles.cache.map(r => r).join(' ').replace('@everyone', ' ')}`)
-        .addField('Server Member Since', `${moment(Target.joinedAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.joinedAt).startOf('day').fromNow()}`)
-        .addField('Discord User Since', `${moment(Target.user.createdAt).format('MMMM Do YYYY, h:mm:ss a')}\n**-** ${moment(Target.user.createdAt).startOf('day').fromNow()}`)
-        .setFooter({text: `Requested By ${interaction.user.tag}`});
+        const Target = interaction.options.getMember("target");
+        let Response = new MessageEmbed()
+            .setAuthor({
+                name: `${Target.user.username}`,
+                iconURL: Target.user.displayAvatarURL({ dynamic: true }),
+            })
+            .setThumbnail(Target.user.displayAvatarURL({ dynamic: true }))
+            .setColor("RANDOM")
+            .addField("UserID", `${Target.user.id}`, false);
+
+        if (
+            Target.roles.cache
+                .map((r) => r)
+                .join(" ")
+                .replace("@everyone", " ") != " "
+        ) {
+            Response.addField(
+                "Roles",
+                `${Target.roles.cache
+                    .map((r) => r)
+                    .join(" ")
+                    .replace("@everyone", " ")}`
+            );
+        } else {
+            Response.addField(
+                "Roles",
+                `No Roles`
+            );
+        }
+        Response.addField(
+            "Server Member Since",
+            `${moment(Target.joinedAt).format(
+                "MMMM Do YYYY, h:mm:ss a"
+            )}\n**-** ${moment(Target.joinedAt).startOf("day").fromNow()}`
+        )
+            .addField(
+                "Discord User Since",
+                `${moment(Target.user.createdAt).format(
+                    "MMMM Do YYYY, h:mm:ss a"
+                )}\n**-** ${moment(Target.user.createdAt)
+                    .startOf("day")
+                    .fromNow()}`
+            )
+            .setFooter({ text: `Requested By ${interaction.user.tag}` });
         interaction.followUp({ embeds: [Response] });
     },
 };
