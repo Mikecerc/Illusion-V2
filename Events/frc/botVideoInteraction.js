36c35b905df7e9c76681269dd81e47866f4f812c
Mikecerc1 <mikecerc1@outlook.com>
2022-07-02 00:25:55 -0400

    oops... accidently left console logs

--- Events/frc/botVideoInteraction.js
+++ Events/frc/botVideoInteraction.js
@@ -2,10 +2,8 @@ const fs = require('fs');
 module.exports = {
     name: 'interactionCreate',
     async execute(interaction) {
-        console.log('ok')
         if (!interaction.isSelectMenu()) return;
         const id = interaction.customId.split('-');
-        console.log(id)
         if (id[0] != 20) return;
         const team = id[1]; 
         fs.readFile("./json/vids.json", (err, res) => {
@@ -14,7 +12,6 @@ module.exports = {
             } else { 
                 const data = JSON.parse(res);
                 const year = interaction.values[0];
-                console.log(year)
                 for (video of data[team][year]) {
                     interaction.update({embeds: [], components: [], content: video})
                 }
