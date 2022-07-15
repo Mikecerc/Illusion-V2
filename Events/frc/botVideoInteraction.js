45884bbe88fe3e3deccf68b393cb55f5afa95c24
Mikecerc1 <mikecerc1@outlook.com>
2022-07-15 03:55:29 -0400

    fixed bug in botvideo and updated packages to fix security issue

--- Events/frc/botVideoInteraction.js
+++ Events/frc/botVideoInteraction.js
@@ -13,7 +13,7 @@ module.exports = {
                 const data = JSON.parse(res);
                 const year = interaction.values[0];
                 for (video of data[team][year]) {
-                    interaction.update({embeds: [], components: [], content: video})
+                    interaction.message.edit({embeds: [], components: [], content: video})
                 }
 
             }
