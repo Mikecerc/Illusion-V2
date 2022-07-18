130ad623c6cd1cb4776c8059976e328dae9e2ea6
Mikecerc1 <mikecerc1@outlook.com>
2022-07-18 15:02:35 -0400

    updated botphoto command to include different extentions from the repo

--- Commands/frc/botphoto.js
+++ Commands/frc/botphoto.js
@@ -15,60 +15,99 @@ export default {
             description: "optionally enter a year",
             type: "NUMBER",
             required: false,
-        }, 
+        },
     ],
     async execute(interaction) {
         await interaction.deferReply();
-        const team = interaction.options.getNumber('team').toString();
-        const year = interaction.options.getNumber('year') != undefined ? interaction.options.getNumber('year').toString(): undefined;
-        let teamNumber = team; 
-            for (let i = 4 - team.length; i > 0; i--) {
-                teamNumber = `0${teamNumber}`;  
-            }
+        const team = interaction.options.getNumber("team").toString();
+        const year =
+            interaction.options.getNumber("year") != undefined
+                ? interaction.options.getNumber("year").toString()
+                : undefined;
+        let teamNumber = team;
+        for (let i = 4 - team.length; i > 0; i--) {
+            teamNumber = `0${teamNumber}`;
+        }
         if (year) {
-            const url = `https://github.com/Team973/RobotPhotos973/raw/master/${year}/${teamNumber}.jpg`
-            const doesExist = await checkStatus(url);   
+            const url = `https://github.com/Team973/RobotPhotos973/raw/master/${year}/${teamNumber}.jpg`;
+            const doesExist = await checkStatus(url);
             if (doesExist) {
                 const response = new MessageEmbed()
-                    .setColor('RANDOM')
-                    .setDescription(`Team ${teamNumber}'s robot for the year ${year}`)
+                    .setColor("RANDOM")
+                    .setDescription(
+                        `Team ${teamNumber}'s robot for the year ${year}`
+                    )
                     .setImage(url)
-                    .setFooter({ text: `Requested By ${interaction.user.tag}` });
+                    .setFooter({
+                        text: `Requested By ${interaction.user.tag}`,
+                    });
                 interaction.followUp({ embeds: [response] });
             } else {
-                const response = new MessageEmbed()
-                    .setColor('RED')
-                    .setDescription(`There is no bot photo for the team ${teamNumber} in the year ${year}`)
-                interaction.followUp({ embeds: [response] });
-            }   
+                const url0 = `https://github.com/Team973/RobotPhotos973/raw/master/${year}/${teamNumber}.JPG`;
+                const doesExist0 = await checkStatus(url0);
+                if (doesExist0) {
+                    const response = new MessageEmbed()
+                        .setColor("RANDOM")
+                        .setDescription(
+                            `Team ${teamNumber}'s robot for the year ${year}`
+                        )
+                        .setImage(url)
+                        .setFooter({
+                            text: `Requested By ${interaction.user.tag}`,
+                        });
+                    interaction.followUp({ embeds: [response] });
+                } else {
+                    const response = new MessageEmbed()
+                        .setColor("RED")
+                        .setDescription(
+                            `There is no bot photo for the team ${teamNumber} in the year ${year}`
+                        );
+                    interaction.followUp({ embeds: [response] });
+                }
+            }
         } else {
             const currentYear = new Date().getFullYear();
             let latestYear;
+            let fileExt;
             for (let i = Number.parseInt(currentYear) - 2016; i > 0; i--) {
                 const testYear = (2016 + i).toString();
-                const url = `https://github.com/Team973/RobotPhotos973/raw/master/${testYear}/${teamNumber}.jpg`
-                const isYear = await checkStatus(url); 
-                if (isYear) {
+                const url = `https://github.com/Team973/RobotPhotos973/raw/master/${testYear}/${teamNumber}.jpg`;
+                const url0 = `https://github.com/Team973/RobotPhotos973/raw/master/${testYear}/${teamNumber}.JPG`;
+                const isYear = await checkStatus(url);
+                const isYear0 = await checkStatus(url0);
+                if (isYear || isYear0) {
                     latestYear = testYear;
+                    if (isYear) {
+                        fileExt = "jpg";
+                    } else if (isYear0) {
+                        fileExt = "JPG";
+                    }
                     break;
-                } 
+                }
             }
+            console.log(fileExt);
             if (latestYear) {
-                const url = `https://github.com/Team973/RobotPhotos973/raw/master/${latestYear}/${teamNumber}.jpg`
+                const url = `https://github.com/Team973/RobotPhotos973/raw/master/${latestYear}/${teamNumber}.${fileExt}`;
                 const response = new MessageEmbed()
-                    .setColor('RANDOM')
-                    .setDescription(`Team ${teamNumber}'s robot for the year ${latestYear}`)
+                    .setColor("RANDOM")
+                    .setDescription(
+                        `Team ${teamNumber}'s robot for the year ${latestYear}`
+                    )
                     .setImage(url)
-                    .setFooter({ text: `Requested By ${interaction.user.tag}` });
+                    .setFooter({
+                        text: `Requested By ${interaction.user.tag}`,
+                    });
                 interaction.followUp({ embeds: [response] });
             } else {
                 const response = new MessageEmbed()
-                    .setColor('RED')
-                    .setDescription(`There is no bot photo for the team ${teamNumber}`)
+                    .setColor("RED")
+                    .setDescription(
+                        `There is no bot photo for the team ${teamNumber}`
+                    );
                 interaction.followUp({ embeds: [response] });
             }
         }
-    }
+    },
 };
 
 async function checkStatus(url) {
