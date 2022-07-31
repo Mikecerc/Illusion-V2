a5a991277bb9f4056a7b743b3d84a53c354b935a
Mikecerc1 <mikecerc1@outlook.com>
2022-07-31 18:43:15 -0400

    re-wrote command handler

--- src/Handlers/Commands.ts
+++ src/Handlers/Commands.ts
@@ -1,5 +1,5 @@
-import { readdirSync } from 'fs';
-export default async (client: any) => {
+import { readdirSync } from "fs";
+/**export default async (client: any) => {
     //create a blank array for commands
     const commandsArry = [];
     //import all command sub-folders
@@ -37,4 +37,41 @@ export default async (client: any) => {
             });
         }
     }
+};
+*/
+export default async (client: any) => {
+    const commandsArry = [];
+    const commandFolders = readdirSync("./build/Commands");
+
+    for (const folder of commandFolders) {
+        const commandFiles = readdirSync(`./build/Commands/${folder}`).filter(
+            (files) => files.endsWith(".js")
+        );
+        for (const file of commandFiles) {
+            const commandFile = await import(`../Commands/${folder}/${file}`);
+            const command = commandFile.default;
+            await client.commands.set(command.name, command);
+            commandsArry.push(command);
+        }
+    }
+    client.on("ready", async () => {
+        const guilds = client.guilds.cache.map((res: any) => {
+            return res;
+        });
+        //test mode determines if the commands will be set globally or locally (by guild)
+        if (process.env.testMode == "true") {
+            //for each guild, set the commands and remove all global commands
+            for (const guild in guilds) {
+                await guilds[guild].commands.set(commandsArry);
+            }
+            await client.application.commands.set([]);
+        } else {
+            //set commands globally and remove all local commands in every guild
+            await client.application.commands.set(commandsArry);
+            for (const guild in guilds) {
+                await guilds[guild].commands.set([]);
+            }
+        }
+    });
+    console.log(`${commandsArry.length} command(s) registered`)
 };
