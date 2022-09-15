2a95c6624c4e5eab188048225e0cd45f6c392c79
Mikecerc1 <mikecerc1@outlook.com>
2022-09-15 16:50:21 -0400

    windows is bad

--- src/commands/Utilities/ping.ts
+++
@@ -1,8 +0,0 @@
-import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
-export default {
-    data: new SlashCommandBuilder().setName("ping").setDescription("Round trip travel between Void and the Discord API"),
-    execute(interaction: any, client: any) {
-        const Response = new EmbedBuilder().setColor("Orange").setDescription(`🏓${client.ws.ping}ms`);
-        interaction.reply({ embeds: [Response] });
-    },
-};
--- src/events/Server/ready.ts
+++
@@ -1,8 +0,0 @@
-import { ActivityType } from "discord.js";
-export default {
-    name: "ready",
-    execute(client: any) {
-        console.log("The client is ready :)");
-        client.user.setActivity("Recycle Rush Videos", { type: ActivityType.Watching });
-    },
-};
