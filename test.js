781192c7dc7771b045ec294b8b1f268464c4008f
Mikecerc1 <mikecerc1@outlook.com>
2022-09-20 15:44:07 -0400

    removed test file

--- test.js
+++
@@ -1,7 +0,0 @@
-import Parser from 'rss-parser';
-const parser = new Parser();
-const url = 'https://www.firstinspires.org/robotics/frc/blog-rss';
-(async () => {
-    const feed = await parser.parseURL(url);
-    console.log(feed.items);
-})();
