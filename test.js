b15a36face2e3bca47bd275d5879505bb8f32ade
Mikecerc1 <mikecerc1@outlook.com>
2022-10-09 13:56:22 -0400

    removed test

--- test.js
+++
@@ -1,6 +0,0 @@
-import translate from 'google-translate-api-x';
-
-const res = await translate('Je Parle la Francais', {to: 'en'});
-
-console.log(res.text); //=> I speak English
-console.log(res.from.language.iso);  //=> nl
